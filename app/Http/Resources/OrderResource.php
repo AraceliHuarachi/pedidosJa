50f0be719478e2a9c8fcb0f38ab4d302995675ac
Araceli HT <araceli@pull.srl>
2024-11-13 10:28:56 -0400

    ORD. Ajuste a Resource, para que solo devuelva el id de Delivery_user y no todo sus datos

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -18,7 +18,7 @@ class OrderResource extends JsonResource
             'id' => $this->id,
             'description' => $this->description,
             'order_date' => $this->order_date,
-            'delivery_user' => new UserResource($this->whenLoaded('deliveryUser')),
+            'delivery_user_id' => $this->whenLoaded('deliveryUser') ? $this->deliveryUser->id : null,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'order_users' => OrderUserResource::collection($this->whenLoaded('orderUsers')),
