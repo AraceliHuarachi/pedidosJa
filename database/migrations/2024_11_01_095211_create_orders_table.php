fe23670bfd33ba95a23dbc291424e31b45a71819
Araceli HT <araceli@pull.srl>
2024-11-14 15:13:09 -0400

    ORD. agregar status a la tabla orders

--- database/migrations/2024_11_01_095211_create_orders_table.php
+++ database/migrations/2024_11_01_095211_create_orders_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('delivery_user_id');
             $table->foreign('delivery_user_id')->references('id')->on('users')->onDelete('cascade');
             $table->date('order_date');
+            $table->string('status')->default('draft');
             $table->timestamps();
         });
     }
