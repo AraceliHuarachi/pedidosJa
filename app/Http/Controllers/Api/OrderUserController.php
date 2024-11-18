a3e79ff8c687146b2e3eccb44ea2ecc461829726
Araceli Torrejon <aracelihtpull@gmail.com>
2024-11-17 20:24:49 -0400

    OU. modif metodo store, solo para registrar order

--- app/Http/Controllers/Api/OrderUserController.php
+++ app/Http/Controllers/Api/OrderUserController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\OrderUser;
-use Illuminate\Http\Request;
 use App\Http\Requests\OrderUserRequest;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\OrderUserResource;
+use App\Models\Order;
 
 class OrderUserController extends Controller
 {
@@ -15,9 +14,27 @@ class OrderUserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(OrderUserRequest $request): OrderUser
+    public function store(OrderUserRequest $request)
     {
-        return OrderUser::create($request->validated());
+        // Acceder a los datos validados
+        $validated = $request->validated();
+
+
+        // Verificar que la orden esté en estado draft o in_process antes de permitir la creación
+        $order = Order::findOrFail($validated['order_id']);
+        if (!in_array($order->state, [Order::STATE_DRAFT, Order::STATE_IN_PROCESS])) {
+            return response()->json(['error' => 'La orden no está en un estado válido para agregar usuarios'], 400);
+        }
+
+        // Crear el registro en order_users
+        $orderUser = OrderUser::create([
+            'order_id' => $validated['order_id'],
+            'user_id' => $validated['user_id'],
+            'amount_money' => null, // Se deja en null por ahora
+        ]);
+
+        // Retornar el ID del registro creado
+        return response()->json(['order_user_id' => $orderUser->id], 201);
     }
 
     /**
