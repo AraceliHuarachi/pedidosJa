a2a29ddf3b6d190d620e1766ad2306aaa05cb014
AraceliHT <araceli@pull.srl>
2024-11-22 15:46:27 -0400

    ORD. cambio en condicional para que el estado en update no sea obligatorio

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -30,16 +30,18 @@ class OrderService
         }
 
         $order->update([
-            'reason' => $data['reason'],
-            'delivery_user_id' => $data['delivery_user_id'],
-            'order_date' => $data['order_date'],
+            'reason' => $data['reason'] ?? $order->reason,
+            'delivery_user_id' => $data['delivery_user_id'] ?? $order->delivery_user_id,
+            'order_date' => $data['order_date'] ?? $order->order_date,
         ]);
 
         // Cambiar el estado si viene explícito en los datos y es válido
-        if (isset($data['state']) && $order->isTransitionValid($data['state'])) {
-            $order->update(['state' => $data['state']]);
-        } else {
-            throw new Exception("Invalid state transition.");
+        if (isset($data['state'])) {
+            if ($order->isTransitionValid($data['state'])) {
+                $order->update(['state' => $data['state']]);
+            } else {
+                throw new Exception("Invalid state transition.");
+            }
         }
 
         return $order;
