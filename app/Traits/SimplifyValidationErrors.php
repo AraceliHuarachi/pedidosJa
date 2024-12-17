8fd0b46855063fc94a6e43c4e5d737085bd7e756
AraceliHT <araceli@pull.srl>
2024-12-17 18:46:30 -0400

    TRAIT. modificacion para implementar con fromRequest y sin form Req

--- app/Traits/SimplifyValidationErrors.php
+++ app/Traits/SimplifyValidationErrors.php
@@ -2,10 +2,16 @@
 
 namespace App\traits;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
 trait SimplifyValidationErrors
 {
+    // Metodo para simplificar los mensajes de error.
     public function simplifyErrorMessages(array $errors)
     {
+        $simplified = [];
+
         foreach ($errors as $key => $messages) {
             // Extraer el nombre del campo después del último punto
             if (preg_match('/\.([^.]+)$/', $key, $matches)) {
@@ -29,5 +35,23 @@ trait SimplifyValidationErrors
         }
 
         return $simplified;
+    }
+    // Metodo para simplificar mensajes de errores dentro de un FormRequest
+    public function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors()->getMessages(); // Obtiene los errores originales
+        $simplifiedErrors = $this->simplifyErrorMessages($errors); // simplifica los mensajes de error
+
+        throw new HttpResponseException(
+            response()->json($simplifiedErrors, 422) // Devuelve los mensajes modificados
+        );
+    }
+    // Metodo para simplificar errores cuando se usa Validator directamente
+    public function forLooseValidations(Validator $validator)
+    {
+        $errors = $validator->errors()->getMessages();
+        $simplifiedErrors = $this->simplifyErrorMessages($errors);
+
+        return response()->json($simplifiedErrors, 422);
     }
 }
