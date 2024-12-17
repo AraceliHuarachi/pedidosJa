a42a52bde3ae78c13dea1960021aef2177695974
AraceliHT <araceli@pull.srl>
2024-12-17 18:47:00 -0400

    CONTRLER. usar el formRequest para probar el trait

--- app/Http/Controllers/ValidationsController.php
+++ app/Http/Controllers/ValidationsController.php
@@ -1,70 +1,21 @@
-<?
+<?php
 
 namespace App\Http\Controllers;
 
-use App\traits\SimplifyValidationErrors;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\ValidationsTestRequest;
 
 class ValidationsController extends Controller
 {
-    use SimplifyValidationErrors;
-
-    public function validateOrdersData(Request $request)
+    public function store(ValidationsTestRequest $request)
     {
+        // En este punto, los datos ya están validados gracias al FormRequest,
+        // Si la validación es exitosa, podemos acceder a los datos validados
+        $validatedData = $request->validated();
 
-        // Datos de ejemplo
-        $data = [
-            'day_date' => null,
-            'orders' => [
-                [
-                    "Order_Nro" => 1,
-                    "date" => "2024-12-11",
-                    "products" => [
-                        ["order_product_id" => 101, "quantity" => 2],
-                        ["order_product_id" => 999, "quantity" => -1], // Error: cantidad negativa
-                    ]
-                ],
-                [
-                    "Order_Nro" => null, // Error: Order_Nro es requerido
-                    "date" => "2024-12-10",
-                    "products" => [
-                        ["order_product_id" => null, "quantity" => 4],
-                        ["order_product_id" => 104, "quantity" => 0.3], //rompe dos reglas
-                    ]
-                ]
-            ]
-        ];
-
-        // Reglas de validación
-        $rules = [
-            'day_date' => 'required',
-            'orders' => 'required|array',
-            'orders.*.Order_Nro' => 'required|integer',
-            'orders.*.date' => 'required|date',
-            'orders.*.products' => 'required|array|min:1',
-            'orders.*.products.*.order_product_id' => 'required|integer', // Validación para ID de producto
-            'orders.*.products.*.quantity' => 'required|integer|min:1', // Validación para cantidad
-        ];
-
-        // validacion 
-        $validator = Validator::make($data, $rules);
-
-        // Verificar si la validación pasó o falló
-        if ($validator->fails()) {
-            // Si falla, almacenar los errores
-            $errors = $validator->errors()->toArray();
-
-            //Simplificar los mensajes de error:
-            $simplifiedErrors = $this->simplifyErrorMessages($errors);
-
-            return response()->json(['errors' => $simplifiedErrors], 400);
-
-            // dd($errors); //ver los errores sin simplificar
-            // dd($simplifiedErrors);
-        }
-        // Si pasa la validación
-        // dd('Los datos son válidos');
-        return response()->json(['message' => 'Datos válidos']);
+        // Si la validación pasa, podemos continuar con la lógica de negocio
+        return response()->json([
+            'message' => 'Validate Data',
+            'data' => $validatedData
+        ], 200);
     }
 }
