baedcfe1d76c4db7f5bf15da675c3e56ef6f32c8
AraceliHT <araceli@pull.srl>
2024-12-18 17:08:58 -0400

    SERVPROV. Incluir mi regla personalizada en el service provider (solo para el ejemplo)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Validator::extend('multiple_of_five', function ($attribute, $value, $parameters, $validator) {
+            return $value % 5 === 0;
+        });
     }
 }
