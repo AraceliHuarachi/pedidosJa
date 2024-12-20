4756d522c809bcdd871c963c7a85e97933a464e5
AraceliHT <araceli@pull.srl>
2024-12-20 17:15:03 -0400

    lang/es -> Movemos el array attributes del archivo validation.php al archivo attributes.php

--- resources/lang/es/attributes.php
+++ resources/lang/es/attributes.php
@@ -11,13 +11,11 @@ return [
     | names, making the validation messages clearer and easier to understand.
     |
     */
-    'attributes' => [
-        'day_date' => 'fecha del día',
-        'orders' => 'pedidos',
-        'Order_Nro' => 'número de orden', // For nested field names
-        'orders.*.date' => 'fecha del pedido',
-        'orders.*.products' => 'productos',
-        'orders.*.products.*.order_product_id' => 'ID del producto',
-        'quantity' => 'cantidad de productos',
-    ]
+    'day_date' => 'fecha del día',
+    'orders' => 'pedidos',
+    'Order_Nro' => 'número de orden', // For nested field names
+    'orders.*.date' => 'fecha del pedido',
+    'orders.*.products' => 'productos',
+    'orders.*.products.*.order_product_id' => 'ID del producto',
+    'quantity' => 'cantidad de productos',
 ];
--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -160,23 +160,4 @@ return [
     // Add custom rule message
     'multiple_of_five' => 'El campo :attribute debe ser un múltiplo de cinco.',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Attributes
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the translated names of form fields.
-    | These translations replace the attribute placeholders with more reader-friendly
-    | names, making the validation messages clearer and easier to understand.
-    |
-    */
-    'attributes' => [
-        'day_date' => 'fecha del día',
-        'orders' => 'pedidos',
-        'Order_Nro' => 'número de orden', // For nested field names
-        'orders.*.date' => 'fecha del pedido',
-        'orders.*.products' => 'productos',
-        'orders.*.products.*.order_product_id' => 'ID del producto',
-        'orders.*.products.*.quantity' => 'cantidad de productos',
-    ]
 ];
