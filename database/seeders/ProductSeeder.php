528d62e19416973e5cba28a8ff1fa78cb8409399
AraceliHT <araceli@pull.srl>
2025-01-03 08:40:45 -0400

    PROD-SEED: cambio en los nombres de productos para probar la traduccion

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,11 +15,11 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         $products = [
-            ['name' => 'Salteña', 'reference_price' => 8.00],
-            ['name' => 'Papitas', 'reference_price' => 30.00],
-            ['name' => 'Rollo de queso', 'reference_price' => 25.00],
-            ['name' => 'Ensalada de Frutas', 'reference_price' => 6.00],
-            ['name' => 'Sandwich de palta', 'reference_price' => 6.00],
+            ['name' => 'Bread', 'reference_price' => 0.50],
+            ['name' => 'Chips', 'reference_price' => 30.00],
+            ['name' => 'Cheese roll', 'reference_price' => 28.00],
+            ['name' => 'Fruit salad', 'reference_price' => 6.00],
+            ['name' => 'Chicken sandwich', 'reference_price' => 6.00],
         ];
 
         foreach ($products as $product) {
